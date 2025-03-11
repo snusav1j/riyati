8c211a76c00ed75deea6f27ec36ebd1dee7c48f5
snusav1j <snusav1j>
2025-03-11 17:47:11 +0300

    update cryptos

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
     if current_user.director?
       User::DIRECTOR_ROLES
     elsif current_user.ceo?
-      User::CEO_ROLES
+      User::DEV_ROLES
     elsif current_user.dev?
       User::DEV_ROLES
     end
